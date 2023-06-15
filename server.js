77510909306382fb90482acca1cacd48e3f22968
ranjan_kuldeep <95350799+ranjankuldeep@users.noreply.github.com>
2023-06-15 20:38:51 +0530

    Bug fixes

--- server.js
+++ server.js
@@ -7,12 +7,7 @@ import http from "http";
 
 import { Server } from "socket.io";
 
-import {
-  userJoin,
-  getCurrentUser,
-  getRoomUsers,
-  userLeave,
-} from "./utils/users.js";
+import { userJoin, getCurrentUser, getRoomUsers, userLeave } from "./utils/users.js";
 
 const app = express();
 const server = http.createServer(app);
@@ -26,46 +21,37 @@ const bot = "chat_BOT";
 
 let user;
 io.on("connection", async (socket) => {
-  logger.info("New WebSocket Connection ....");
-  socket.on("joinRoom", async ({ username, room }) => {
-    user = await userJoin(socket.id, username, room);
+	logger.info("New WebSocket Connection ....");
+	socket.on("joinRoom", async ({ username, room }) => {
+		user = await userJoin(socket.id, username, room);
 
-    socket.join(user.room);
+		socket.join(user.room);
 
-    socket.emit("loadChats", await loadChats(user.roomId));
+		socket.emit("loadChats", await loadChats(user.roomId));
 
-    socket.broadcast
-      .to(user.room)
-      .emit(
-        "message",
-        messageFormatter(bot, `${user.username} has joined the chat`)
-      );
-    io.to(user.room).emit("roomUsers", {
-      room: user.room,
-      users: await getRoomUsers(user.roomId),
-    });
-  });
-  socket.on("disconnect", async () => {
-    const user = await userLeave(socket.id);
-    io.to(user.room).emit(
-      "message",
-      messageFormatter(bot, `${user.username} has left the chat`)
-    );
-    io.to(user.room).emit("roomUsers", {
-      room: user.room,
-      users: await getRoomUsers(user.roomId),
-    });
-  });
-  socket.on("chatMessage", async (msg) => {
-    const user = getCurrentUser(socket.id);
+		socket.broadcast
+			.to(user.room)
+			.emit("message", messageFormatter(bot, `${user.username} has joined the chat`));
+		io.to(user.room).emit("roomUsers", {
+			room: user.room,
+			users: await getRoomUsers(user.roomId),
+		});
+	});
+	socket.on("disconnect", async () => {
+		const user = await userLeave(socket.id);
+		io.to(user.room).emit("message", messageFormatter(bot, `${user.username} has left the chat`));
+		io.to(user.room).emit("roomUsers", {
+			room: user.room,
+			users: await getRoomUsers(user.roomId),
+		});
+	});
+	socket.on("chatMessage", async (msg) => {
+		const user = getCurrentUser(socket.id);
 
-    io.to(user.room).emit(
-      "message",
-      await chatMessage(user.roomId, user.username, msg)
-    );
-  });
+		io.to(user.room).emit("message", await chatMessage(user.roomId, user.username, msg));
+	});
 });
 
-server.listen(8080 || process.env.PORT, () => {
-  logger.info("App is listening on Port 8080");
+server.listen(8000 || process.env.PORT, () => {
+	logger.info("App is listening on Port 8080");
 });
